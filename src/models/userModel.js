acd72d9af0d4023b5e17aa38a4ef8eb138eda8cd
rammar18 <ammar18rafi@gmail.com>
2024-06-08 00:40:51 +0700

    add minor update and findUserById for verifyToken

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -7,7 +7,6 @@ async function hashPassword(password) {
   return await bcrypt.hash(password, saltRounds);
 }
 
-// async function createUser(email, password, name, age, gender) {
 async function createUser(email, password) {
   const hashedPassword = await hashPassword(password);
   const result = await pool.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, hashedPassword]);
@@ -20,28 +19,18 @@ async function findUserByEmail(email) {
   return rows[0];
 }
 
-// async function findUserById(userId) {
-//   const [rows] = await pool.query('SELECT * FORM users WHERE id = ?', [userId]);
-//   return rows[0];
-// }
+async function findUserById(userId) {
+  const [rows] = await pool.query('SELECT * FROM users WHERE id = ?', [userId]);
+  return rows[0];
+}
 
 async function verifyPassword(inputPassword, storedPassword) {
   return await bcrypt.compare(inputPassword, storedPassword);
 }
 
-// async function setHasBMIEntry(userId) {
-//   try {
-//     await pool.query('UPDATE users SET has_bmi_entry = true WHERE id = ?', userId);
-//     console.log('User marked as having BMI entry');
-//   } catch (error) {
-//     console.error('Error making user as having BMI entry', error);
-//   }
-// }
-
 module.exports = {
   createUser,
   findUserByEmail,
-  // findUserById,
   verifyPassword,
-  // setHasBMIEntry,
+  findUserById,
 };
