3384aa5c65049db651f77d7f1f4df90097973945
rammar18 <ammar18rafi@gmail.com>
2024-06-08 17:56:38 +0700

    rename host

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
-    host: 'localhost',
+    host: '0.0.0.0',
   });
 
   await server.register(hapiAuthJwt2);
