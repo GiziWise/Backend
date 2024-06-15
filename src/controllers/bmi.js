766ae4a0af97674595565c7886dab21a311091eb
rammar18 <ammar18rafi@gmail.com>
2024-06-15 12:56:38 +0700

    (refactor) error handling data validation

--- src/controllers/bmi.js
+++ src/controllers/bmi.js
@@ -4,97 +4,120 @@ const bmiModel = require('../models/bmiModel');
 
 // data validation request bmi
 const bmiSchema = Joi.object({
-  weight: Joi.number().positive().required(),
-  height: Joi.number().positive().required(),
-  gender: Joi.string().valid('male', 'female').required(),
-  dob: Joi.date().iso().required(),
+  weight: Joi.number().positive().required().messages({
+    'number.positive': 'Must be a positive number',
+    'any.required': 'Weight is required',
+  }),
+  height: Joi.number().positive().required().messages({
+    'number.positive': 'Must be a positive number',
+    'any.required': 'Height is required',
+  }),
+  gender: Joi.string().valid('male', 'female').required().messages({
+    'any.only': 'Must be male or female',
+    'any.required': 'Gender is required',
+  }),
+  dob: Joi.date().iso().required().messages({
+    'date.format': 'Must be a valid date of birth',
+    'any.required': 'Date of birth is required',
+  }),
 });
 
 // Function calculate BMI
 async function calculateBmi(request, h) {
-  const { error } = bmiSchema.validate(request.payload);
+//  const { error } = bmiSchema.validate(request.payload);
+//
+//  if (error) {
+//    return h.response({ error: error.details[0].message }).code(400);
+//  }
+//
+//  // Take data request
+//  const {
+//    weight, height, gender, dob,
+//  } = request.payload;
+  try {
+    const {
+      weight, height, gender, dob,
+    } = await bmiSchema.validateAsync(request.payload, { abortEarly: false });
 
-  if (error) {
-    return h.response({ error: error.details[0].message }).code(400);
+    // Formula BMI
+    const heightInMeters = height / 100; // assuming height is provided in cm
+    const bmi = weight / (heightInMeters * heightInMeters);
+    const bmiResult = Math.round(bmi * 10) / 10;
+
+    // Calculate age
+    const birthDate = new Date(dob);
+    const age = new Date().getFullYear() - birthDate.getFullYear();
+
+    // Determine BMI category
+    let category;
+    if (bmi < 18.5) {
+      category = 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      category = 'Normal';
+    } else if (bmi >= 25 && bmi < 29.9) {
+      category = 'Overweight';
+    } else {
+      category = 'Obese';
+    }
+
+    // Calculate healthy weight range
+    const minHealthyWeight = 18.5 * (heightInMeters * heightInMeters);
+    const maxHealthyWeight = 24.9 * (heightInMeters * heightInMeters);
+    const roundedMinHealthyWeight = Math.round(minHealthyWeight * 10) / 10;
+    const roundedMaxHealthyWeight = Math.round(maxHealthyWeight * 10) / 10;
+
+    // Format healthy weight range as a string
+    const healthyWeightRange = `${roundedMinHealthyWeight} kg - ${roundedMaxHealthyWeight} kg`;
+
+    // Calculate BMR calory
+    let bmr;
+    if (gender === 'male') {
+      bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+    } else {
+      bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
+    }
+
+    // Total Daily Energy Expenditure harian, minimal bergerak atau kerja
+    const tee = bmr * 1.2;
+    const calory = Math.round(tee * 10) / 10;
+
+    // Save BMI data to the database with token cookie
+    const { userId } = request.auth.credentials;
+    if (!userId) {
+      return h.response({ status: 'fail', message: 'User not authenticated.' }).code(401);
+    }
+    const bmiData = {
+      bmi: bmiResult,
+      category,
+      weight,
+      height,
+      dob,
+      age,
+      gender,
+      healthyWeightRange,
+      calory,
+    };
+    const saveResult = await bmiModel.saveBmiData(userId, bmiData);
+
+    // Check if saving BMI data was successful
+    if (!saveResult.success) {
+      console.error('Failed to save BMI data to the database:', saveResult.message);
+      // Handle error response
+      return h.response({ status: 'fail', message: 'Failed to save BMI data.' }).code(500);
+    }
+
+    // Return success response
+    return h.response({
+      status: 'success',
+      message: 'BMI data saved successfully',
+    }).code(200);
+  } catch (e) {
+    if (e.isJoi) {
+      return h.response({ status: 'fail', message: e.message }).code(400);
+    }
+    console.error('Error calculating BMI', e.message);
+    return h.response({ status: 'fail', message: 'Failed to calculate BMI' }).code(500);
   }
-
-  // Take data request
-  const {
-    weight, height, gender, dob,
-  } = request.payload;
-
-  // Formula BMI
-  const heightInMeters = height / 100; // assuming height is provided in cm
-  const bmi = weight / (heightInMeters * heightInMeters);
-  const bmiResult = Math.round(bmi * 10) / 10;
-
-  // Calculate age
-  const birthDate = new Date(dob);
-  const age = new Date().getFullYear() - birthDate.getFullYear();
-
-  // Determine BMI category
-  let category;
-  if (bmi < 18.5) {
-    category = 'Underweight';
-  } else if (bmi >= 18.5 && bmi < 24.9) {
-    category = 'Normal';
-  } else if (bmi >= 25 && bmi < 29.9) {
-    category = 'Overweight';
-  } else {
-    category = 'Obese';
-  }
-
-  // Calculate healthy weight range
-  const minHealthyWeight = 18.5 * (heightInMeters * heightInMeters);
-  const maxHealthyWeight = 24.9 * (heightInMeters * heightInMeters);
-  const roundedMinHealthyWeight = Math.round(minHealthyWeight * 10) / 10;
-  const roundedMaxHealthyWeight = Math.round(maxHealthyWeight * 10) / 10;
-
-  // Format healthy weight range as a string
-  const healthyWeightRange = `${roundedMinHealthyWeight} kg - ${roundedMaxHealthyWeight} kg`;
-
-  // Calculate BMR calory
-  let bmr;
-  if (gender === 'male') {
-    bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
-  } else {
-    bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
-  }
-
-  // Total Daily Energy Expenditure harian, minimal bergerak atau kerja
-  const tee = bmr * 1.2;
-  const calory = Math.round(tee * 10) / 10;
-
-  // Save BMI data to the database with token cookie
-  const { userId } = request.auth.credentials;
-  if (!userId) {
-    return h.response({ status: 'fail', message: 'User not authenticated.' }).code(401);
-  }
-  const bmiData = {
-    bmi: bmiResult,
-    category,
-    weight,
-    height,
-    dob,
-    age,
-    gender,
-    healthyWeightRange,
-    calory,
-  };
-  const saveResult = await bmiModel.saveBmiData(userId, bmiData);
-
-  // Check if saving BMI data was successful
-  if (!saveResult.success) {
-    console.error('Failed to save BMI data to the database:', saveResult.message);
-    // Handle error response
-    return h.response({ status: 'fail', message: 'Failed to save BMI data.' }).code(500);
-  }
-
-  // Return success response
-  return h.response({
-    status: 'success',
-    message: 'BMI data saved successfully',
-  }).code(200);
 }
 
 // Get all data BMI
