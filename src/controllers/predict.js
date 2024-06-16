be31676a6d384b6276fe5fd53ccdf566fd0a1e60
rammar18 <ammar18rafi@gmail.com>
2024-06-16 19:54:36 +0700

    (feat) new function predict food can store to db and get data result

--- src/controllers/predict.js
+++ src/controllers/predict.js
@@ -1,8 +1,9 @@
 /* eslint-disable camelcase */
 /* eslint-disable import/no-extraneous-dependencies */
 const axios = require('axios');
+const predictModel = require('../models/predictModel');
 
-async function predictModel(request, h) {
+async function addPredict(request, h) {
   const { nama_makanan, portion_size } = request.payload;
   const url = 'https://asia-southeast2-capstone-ch241-ps176.cloudfunctions.net/predict';
 
@@ -19,6 +20,28 @@ async function predictModel(request, h) {
       lemak: LEMAK,
       protein: PROTEIN,
     };
+
+    // simpan hasil prediksi ke database
+    const { userId } = request.auth.credentials;
+    if (!userId) {
+      return h.response({ status: 'fail', message: 'User not found' }).code(401);
+    }
+    const predictionData = {
+      nama_makanan,
+      portion_size,
+      energi: ENERGI,
+      lemak: LEMAK,
+      protein: PROTEIN,
+    };
+
+    const saveResult = await predictModel.savePrediction(userId, predictionData);
+
+    // Check if saving prediction data was successful
+    if (!saveResult.success) {
+      console.error('Failed to save prediction data to the database:', saveResult.message);
+      return h.response({ status: 'fail', message: 'Failed to save prediction data.' }).code(500);
+    }
+
     return h.response({
       status: 'success',
       dataprediksi: formatResponse,
@@ -28,4 +51,30 @@ async function predictModel(request, h) {
   }
 }
 
-module.exports = { predictModel };
+async function getPredictions(request, h) {
+  try {
+    const { user } = request;
+    if (!user) {
+      return h.response({ status: 'fail', message: 'User not found' }).code(404);
+    }
+
+    const predictions = await predictModel.getPredictionsByUserId(user.id);
+
+    const formattedPredictions = predictions.map((prediction) => ({
+      nama_makanan: prediction.nama_makanan,
+      energi: prediction.energi,
+      lemak: prediction.lemak,
+      protein: prediction.protein,
+    }));
+
+    return h.response({
+      status: 'success',
+      datamakanan: formattedPredictions,
+    }).code(200);
+  } catch (error) {
+    console.error('Error fetching predictions:', error.message); // eslint-disable-line no-console
+    return h.response({ status: 'fail', message: 'Failed to fetch predictions' }).code(500);
+  }
+}
+
+module.exports = { addPredict, getPredictions };
